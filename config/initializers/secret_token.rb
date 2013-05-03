e7b4d948d81b1d08ed67dfd93bf6cb700f4432f6
Eric Liu <dathtohn@gmail.com>
2013-05-03 12:11:37 -0400

    first commit - set up twilio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmsJournal::Application.config.secret_token = 'af5ff793bad7666e7bf43245ee09cb9ad58e5ce53da316ef6e94e25324b4048fd912800cebcbc9d015803a23e3675c3619f8b28e2259c96325c97a1366caeef3'
